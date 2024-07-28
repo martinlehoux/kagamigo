b690037447760694045375bef271449cca1c8032
Martin Kagamino Lehoux <martin@lehoux.net>
2024-07-28 16:30:48 +0200

    feat(kcore): Add Assert

--- kcore/error.go
+++ kcore/error.go
@@ -1,6 +1,9 @@
 package kcore
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func Wrap(err error, msg string) error {
 	return fmt.Errorf("%s: %w", msg, err)
@@ -10,6 +13,18 @@ func Expect(err error, msg string) {
 	if err != nil {
 		if msg != "" {
 			err = Wrap(err, msg)
+		}
+		panic(err)
+	}
+}
+
+var ErrAssert = errors.New("assertion error")
+
+func Assert(cond bool, msg string) {
+	if !cond {
+		err := ErrAssert
+		if msg != "" {
+			err = fmt.Errorf("%w: %s", err, msg)
 		}
 		panic(err)
 	}
