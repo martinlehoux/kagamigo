2d6e6fad45057b829e84375e914e3301c4bd73ba
Martin Kagamino Lehoux <martin@lehoux.net>
2024-09-28 11:26:16 +0200

    chore(deps): Manage CI deps

--- ci-deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ev
-
-go install github.com/fzipp/gocyclo/cmd/gocyclo@latest
-go install honnef.co/go/tools/cmd/staticcheck@latest
-go install github.com/securego/gosec/v2/cmd/gosec@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
